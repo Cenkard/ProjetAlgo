e59a9c61f04fd79c4bef7c73d800c75815ab9883
Omar <omartot@hotmail.com>
2021-11-21 00:15:42 +0100

    AlgoGlouton

--- Implementation.py
+++ Implementation.py
@@ -5,6 +5,7 @@ import sys
 inf= sys.maxint
 S=7
 V=[1,2,4,6,9]
+
 def AlgoOptimise(S, V):
 	k = len(V)
 	M = [[0 for x in range(k+1)] for y in range(S+1)]
@@ -44,6 +45,33 @@ def AlgoOptimiseTab(S,V):
 				M[s][i] = terme1+[0],n1
 			#print(M[s][i])
 	return M
+
+"""
+def AlgoOptimiseTab2(S, V):
+	k = len(V)
+	M = [[([0 for l in range(x)],0)for x in range(k+1)] for y in range(S+1)] 
+
+	for s in range(1, S+1):
+		M[s][0] = [],inf
+"""
+
+def AlgoGlouton(S, V):
+	L= len(V)
+	A = [0 for i in range(L)]
+	St = S
+	if (S=0):
+		return A
+	elif (S<0 or L=0):
+		return "Erreur, S<0 ou V=[]"
+	for i in range(L-1,-1, -1):
+		ResDiv = St/V[i]
+		if (ResDiv>=1):
+			St = St-ResDiv*V[i]
+			A[i] = ResDiv
+	return A
+
+
+print(AlgoGlouton(S, V))
 
 M = AlgoOptimiseTab(S, V)
 for el in M:
